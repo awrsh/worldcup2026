67c23885e44f43026ec82139a9399ba6d1be2454
awrsh <arashzarandi@gmail.com>
2026-05-25 10:01:58 +0330

    Refactor import order in main.ts to maintain consistency and improve readability

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
-import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
